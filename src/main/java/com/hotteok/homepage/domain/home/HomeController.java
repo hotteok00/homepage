ac7a2629da8db3dd0fbb55fad77432ea309f2a45
hotteok00 <hojun7889@gmail.com>
2024-08-28 23:04:09 +0900

    HP-26 Home / Feat : initialization
    controller, service
    
    Ref : #26

--- src/main/java/com/hotteok/homepage/domain/home/HomeController.java
+++ src/main/java/com/hotteok/homepage/domain/home/HomeController.java
@@ -1,8 +1,21 @@
 package com.hotteok.homepage.domain.home;
 
+import com.hotteok.homepage.global.response.Response;
+import com.hotteok.homepage.global.response.ResponseCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/home")
+@RequiredArgsConstructor
 public class HomeController {
+    private final HomeService homeService;
 
+    @GetMapping()
+    public ResponseEntity<Response> home() {
+        return ResponseEntity.ok(Response.of(ResponseCode.SUCCESS));
+    }
 }
